19fcae5ae30efc2c8210e17467ef9cb7e892a068
Frank Schumann <fjahn78+gihub@gmail.com>
2015-05-10 00:30:54 +0200

    Add styles

--- templates/template_tut/index.php
+++ templates/template_tut/index.php
@@ -1,8 +1,11 @@
 <?php
 
+defined ('_JEXEC') or die;
+
 $doc = JFactory::getDocument();
 
 $doc->addStyleSheet($this->baseurl . '/media/jui/css/bootstrap.min.css');
+$doc->addStyleSheet($this->baseurl - '/media/jui/css/bootstrap-responsive.css');
 $doc->addStyleSheet('templates/' . $this->template . '/css/style.css');
 $doc->addScript('/templates/' . $this->template . '/js/main.js', 'text/javascript');
 
@@ -10,34 +13,39 @@ $doc->addScript('/templates/' . $this->template . '/js/main.js', 'text/javascrip
 <!DOCTYPE html>
 <html>
 <head>
-  <jdoc:include type="head" />
+<jdoc:include type="head" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0"
+
 </head>
 <body>
 <!-- main container -->
-<div class='main_container'> 
-  <!-- header -->
-  <div class='header'>Header</div>
+<div class='container'>
+<!-- header -->
+<div class='row'>
+  <div class='span12'>Header</div>
+</div>
+
+<!-- mid container - includes main content area and right sidebar -->
+<div class='row'> 
   
-  <!-- mid container - includes main content area and right sidebar -->
-  <div class='mid_container'> 
-    
-    <!-- main content area -->
-    <div class='main_content_area'>
-      <jdoc:include type="modules" name="position-3" style="xhtml" />
-      <jdoc:include type="message" />
-      <jdoc:include type="component" />
-      <jdoc:include type="modules" name="position-2" style="none" />
-    </div>
-    
-    <!-- right sidebar -->
-    <div class='right_sidebar'>
-      <jdoc:include type="modules" name="position-7" style="well" />
-    </div>
-    <div style='clear:both;'></div>
+  <!-- main content area -->
+  <div class='span9'>
+    <jdoc:include type="modules" name="position-3" style="xhtml" />
+    <jdoc:include type="message" />
+    <jdoc:include type="component" />
+    <jdoc:include type="modules" name="position-2" style="none" />
   </div>
   
-  <!-- footer -->
-  <div class='footer'> Footer </div>
+  <!-- right sidebar -->
+  <div class='span3'>
+    <jdoc:include type="modules" name="position-7" style="well" />
+  </div>
+  <div style='clear:both;'></div>
+</div>
+
+<!-- footer -->
+<div class="row">
+  <div class="span12"> Footer </div>
 </div>
 </body>
 </html>
